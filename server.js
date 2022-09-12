c17dd497dc95e0c2efec90a80017599e6ce683cf
enceps <spence.d.cole@gmail.com>
2022-09-12 01:47:19 -0400

    add ability to create and save notes

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
-
 const  notes  = require('./db/db.json')
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/api/notes', (req,res) => {
     let results = notes;
     console.log(req.query)
-    res.json(results);
+    res.json(results.slice(1));
     console.log(notes)
 });
 
@@ -20,6 +21,43 @@ app.get('api/notes/:id', (req,res) => {
     } else {
         res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+};
+
+app.post('/api/notes', (req,res) => {
+const newNote = createNewNote(req.body, notes);
+res.json(newNote);
 });
 
 function findById(id, notesArray) {
