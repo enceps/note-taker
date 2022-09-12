20d713e68a235e123ed846d7040bb2a45ac2bdf7
enceps <spence.d.cole@gmail.com>
2022-09-11 21:24:28 -0400

    add Heroku

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
 
+const  notes  = require('./db/db.json')
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+
+app.get('/api/notes', (req,res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+    console.log(notes)
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
